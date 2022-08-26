68b50dc7cc0125f880bcf71161fe1c2a755df4e4
Joseph Osako <josephosako@gmail.com>
2022-08-25 22:40:04 -0400

    Added mem_map code.

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,7 @@
 /* kernel.c */
+#include <stdint.h>
 #include "terminal.h"
+#include "mem_map.h"
 
 #define KDATA_OFFSET 0xc000fffc
 
@@ -8,8 +10,10 @@ enum KData
     mmap_cnt = 0,
     mmap = mmap_cnt + 1 + (24 * 16),
     drive_id,
-    fat
+    fat = drive_id + 1 + (9 * (512 / 4))
 };
+
+
 
 
 void kernel_main()
@@ -18,9 +22,16 @@ void kernel_main()
     kprints("Starting Kernel...\n\n", CYAN, BLACK, 0);
 
     uint32_t* fs_data = (uint32_t *) KDATA_OFFSET;
-    kprints("Drive ID: ", WHITE, BLACK, 0);
-    kprintu(*(fs_data - mmap_cnt), 16, WHITE, BLACK, 0);
-    kprints("\n\nMemory map (", WHITE, BLACK, 0);
-    kprintu(*(fs_data - mmap_cnt), 16, WHITE, BLACK, 0);
-    kprints(" entries):\n", WHITE, BLACK, 0);
+
+    kprints("Passed Data offset ", WHITE, BLACK, 0);
+    kprintu((uint32_t)fs_data, 16, WHITE, BLACK, 0);
+
+    kprints("\nDrive ID: ", WHITE, BLACK, 0);
+    kprintu(*(fs_data - drive_id), 16, WHITE, BLACK, 0);
+    print_mmap(*(fs_data - mmap_cnt), (struct memory_map_entry*)(fs_data - mmap));
 }
+
+
+
+
+
