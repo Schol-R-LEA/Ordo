27227070b64773aa72c6029f4ed05fd5c98358cb
Joseph Osako <josephosako@gmail.com>
2023-01-23 14:12:48 -0500

    set up kernel stack, working on paging

--- src/kstart.asm
+++ src/kstart.asm
@@ -1,9 +1,13 @@
 
    extern kernel_main
+   extern kernel_stack_top
    global kstart
 
 [SECTION .text]
 kstart:
+        mov eax, kernel_stack_top
+        mov esp, eax
+
         call kernel_main
 
    .halted_loop:
